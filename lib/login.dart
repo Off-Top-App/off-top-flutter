95e174b3d075c5bf35255c23f52f379263781bbe
Tony <tonynguyen0925@gmail.com>
2020-06-12 00:26:10 -0700

    added temporary connection the offtop.user database

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,7 @@ import 'package:off_top_mobile/components/offTopTitle.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:off_top_mobile/components/footer/bottomNavigationTabs.dart';
+import 'package:off_top_mobile/settings_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,6 +25,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController loginController = TextEditingController();
   int userId;
   int loginAttempts = 0;
+  String userEmail;
 
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -56,9 +58,22 @@ class _LoginPageState extends State<LoginPage> {
     return user;
   }
 
-  void onGoogleSignIn(BuildContext context) async {
+  Future<void> onGoogleSignIn(BuildContext context) async {
     FirebaseUser user = await _handleSignIn();
-    Navigator.push(context, MaterialPageRoute(builder: (context) => null));
+    userEmail = user.email;
+    if (userId == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => BottomNavigationTabs(
+          RecordingPage(userId: userId),
+        ), //changed route to include initial page
+      ),
+    );
   }
 
   @override
@@ -72,6 +87,7 @@ class _LoginPageState extends State<LoginPage> {
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             onPressed: () {
+              makeLoginRequest();
               onGoogleSignIn(context);
             },
             color: Colors.blueAccent,
@@ -92,115 +108,8 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-/*
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      appBar: offTopTitle(context),
-      body: Center(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'Off-Top Login',
-                    style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w800,
-                        color: Theme.of(context).accentColor),
-                  ),
-                  const SizedBox(height: 20.0),
-                  usernameField(),
-                  const SizedBox(height: 10.0),
-                  passwordField(),
-                  isEmailValid(),
-                  const SizedBox(height: 15.0),
-                  loginButton(context),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      forgotPassword(context),
-                      createAccount(context),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget isEmailValid() {
-    if (userId == null && loginAttempts > 0) {
-      return Padding(
-        padding: const EdgeInsets.only(top: 8.0),
-        child: Text(
-          'Invalid Email. Try again!',
-          style: TextStyle(color: Colors.red),
-        ),
-      );
-    } else {
-      return const Text('');
-    }
-  }
-
-  Widget usernameField() {
-    return TextField(
-      style: TextStyle(color: Theme.of(context).accentColor),
-      controller: loginController,
-      obscureText: false,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: 'Username',
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-  }
-
-  Widget passwordField() {
-    return TextField(
-      style: TextStyle(color: Theme.of(context).accentColor),
-      obscureText: true,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: 'Password',
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-  }
-
-  Widget forgotPassword(BuildContext context) {
-    return FlatButton(
-      child: Text(
-        'forgot password?',
-        style: TextStyle(color: Theme.of(context).accentColor),
-      ),
-      onPressed: () {
-        Navigator.pushNamed(context, RecordingRoute);
-      },
-    );
-  }
-
-  Widget createAccount(BuildContext context) {
-    return FlatButton(
-      child: Text(
-        'create account',
-        style: TextStyle(color: Theme.of(context).accentColor),
-      ),
-      onPressed: () {
-        Navigator.pushNamed(context, RecordingRoute);
-      },
-    );
-  }
-*/
   Future<void> makeLoginRequest() async {
-    final String userEmail = loginController.text;
+    final String userEmail = this.userEmail;
     final String url = 'http://localhost:9000/user/$userEmail';
     //String url = 'http://10.0.2.2:9000/user/${userEmail}/';
     final http.Response response = await http.get(Uri.encodeFull(url),
