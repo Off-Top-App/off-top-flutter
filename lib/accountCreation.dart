ad027dc81529b4a54a44ae42d40ecb9334633f49
Tony <tony.tong.210@my.csun.edu>
2020-06-22 17:14:28 -0700

    Cleaned code and added dynamic email and date creation.

--- lib/accountCreation.dart
+++ lib/accountCreation.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 //import 'package:off_top_mobile/lib/login.dart'; // will edit to sign-up page
@@ -10,35 +9,26 @@ import 'package:off_top_mobile/login.dart';
 
 int repoNumber;
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
+  const SignUp({@required this.email});
+  final String email;
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Sign-Up Page')),
-            body: Center(child: TransferData())));
-  }
+  _SignUpState createState() => _SignUpState();
 }
 
-class TransferData extends StatefulWidget {
-  @override
-  TransferDataWidget createState() => TransferDataWidget();
-}
-
-class TransferDataWidget extends State {
-// Getting value from TextField widget.
+class _SignUpState extends State<SignUp> {
+  // Getting value from TextField widget.
   //final idController = TextEditingController();
-  final firstNameController = TextEditingController();
-  final lastNameController = TextEditingController();
-  final cityController = TextEditingController();
-  final ageController = TextEditingController();
-  final genderController = TextEditingController();
-  final professionalController = TextEditingController();
-  final usernameController = TextEditingController();
-  final emailController = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController genderController = TextEditingController();
+  final TextEditingController professionalController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
 
   bool visible = false;
-  Future makePostRequest() async {
+  Future<void> makePostRequest() async {
     setState(() {
       visible = true;
     });
@@ -46,40 +36,45 @@ class TransferDataWidget extends State {
 
     // Getting value from Controller
     //String Id = idController.text;
-    String firstName = firstNameController.text;
-    String lastName = lastNameController.text;
-    String city = cityController.text;
-    String age = ageController.text;
-    String gender = genderController.text;
-    String professional = professionalController.text;
-    String username = usernameController.text;
-    String email = emailController.text;
+    final String firstName = firstNameController.text;
+    final String lastName = lastNameController.text;
+    final String city = cityController.text;
+    final String age = ageController.text;
+    final String gender = genderController.text;
+    final String professional = professionalController.text;
+    final String username = usernameController.text;
+    final DateTime now = DateTime.now();
+    final String formatDate =
+        '${now.month.toString()}/${now.day.toString()}/${now.year.toString()}';
+    //String email = emailController.text;
 
     // API
-    String address = 'http://localhost:9000/setUser';
-    Map<String, String> headers = {'Content-type': 'application/json'};
-    var data = {
-      "id": 1,
-      "age": age,
-      "city": city,
-      "firstName": firstName,
-      "lastName": lastName,
-      "gender": gender,
-      "professional": professional,
-      "email": email,
-      "username": username,
-      "password": "HoldTheDoor",
-      "createdAt": "04/17/2011",
-      "deletedAt": "05/19/2019",
+    const String address = 'http://localhost:9000/setUser';
+    final Map<String, String> headers = <String, String>{
+      'Content-type': 'application/json'
+    };
+    final dynamic data = {
+      'id': 1,
+      'age': age,
+      'city': city,
+      'firstName': firstName,
+      'lastName': lastName,
+      'gender': gender,
+      'professional': professional,
+      'email': widget.email,
+      'username': username,
+      'password': 'HoldTheDoor',
+      'createdAt': formatDate,
+      'deletedAt': 'null',
     };
 
     // Make respone
     //final http.Response response = await http.get(Uri.encodeFull(url),
-    var call = await http.post(address,
+    final http.Response call = await http.post(address,
         headers: headers, body: json.encode(data)); // webcall
 
-    var message = jsonDecode(call.body); // API response
-    int check = call.statusCode;
+    //final dynamic message = jsonDecode(call.body); // API response
+    final int check = call.statusCode;
     if (check == 200) {
       debugPrint('call accepted');
     } else {
@@ -91,10 +86,10 @@ class TransferDataWidget extends State {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: new Text("Welcome"),
+          title: const Text('Welcome'),
           actions: <Widget>[
             FlatButton(
-              child: new Text("OK"),
+              child: const Text('OK'),
               onPressed: () {
                 Navigator.pushAndRemoveUntil(
                     context,
@@ -112,109 +107,111 @@ class TransferDataWidget extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: const Text('Sign-Up Page')),
         body: SingleChildScrollView(
             child: Center(
-      child: Column(
-        children: <Widget>[
-          Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text('Fill All Information in Form',
-                  style: TextStyle(fontSize: 22))),
-          Container(
-              // will delete, this is for testing
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: emailController,
-                autocorrect: true,
-                keyboardType: TextInputType.emailAddress,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter Email'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: firstNameController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter First Name Here'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: lastNameController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter Last Name Here'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: ageController,
-                autocorrect: true,
-                keyboardType: TextInputType.number,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter Age'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: cityController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter City Here'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: genderController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter Gender'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: professionalController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Enter professional'),
-              )),
-          Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                controller: usernameController,
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                //textCapitalization: TextCapitalization.sentences,
-                decoration: InputDecoration(hintText: 'Create a username'),
-              )),
-          RaisedButton(
-            onPressed: makePostRequest,
-            color: Colors.pink,
-            textColor: Colors.white,
-            padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
-            child: Text('Click Here To Submit Data To Server'),
+          child: Column(
+            children: <Widget>[
+              const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text('Fill All Information in Form',
+                      style: TextStyle(fontSize: 22))),
+              Container(
+                // for testing
+                width: 280.0,
+                padding: const EdgeInsets.all(10.0),
+                child: Text('email: ' + widget.email,
+                    style: const TextStyle(fontSize: 15.0)),
+              ),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: firstNameController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter First Name Here'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: lastNameController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter Last Name Here'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: ageController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.number,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(hintText: 'Enter Age'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: cityController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter City Here'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: genderController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(hintText: 'Enter Gender'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: professionalController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter professional'),
+                  )),
+              Container(
+                  width: 280.0,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: usernameController,
+                    autocorrect: true,
+                    keyboardType: TextInputType.text,
+                    //textCapitalization: TextCapitalization.sentences,
+                    decoration:
+                        const InputDecoration(hintText: 'Create a username'),
+                  )),
+              RaisedButton(
+                onPressed: makePostRequest,
+                color: Colors.pink,
+                textColor: Colors.white,
+                padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                child: const Text('Click Here To Submit Data To Server'),
+              ),
+              Visibility(
+                  visible: visible,
+                  child: Container(
+                      margin: const EdgeInsets.only(bottom: 30),
+                      child: const CircularProgressIndicator())),
+            ],
           ),
-          Visibility(
-              visible: visible,
-              child: Container(
-                  margin: EdgeInsets.only(bottom: 30),
-                  child: CircularProgressIndicator())),
-        ],
-      ),
-    )));
+        )));
   }
 }
